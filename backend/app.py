36f9958f3f782ce3104349c1c3dee5e4ac834f9f
Herbert Rafael Reyes Portillo <36286254+herbertreyes13j@users.noreply.github.com>
2020-10-30 22:27:29 -0600

    Back end terminado

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,53 @@
 from flask import Flask, request
 from flask_cors import CORS
-from mysql import Mysql
-import simplejson as json
+import json
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = '34.72.187.239'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'Sopes1'
+
+mysql = MySQL(app)
 cors = CORS(app, resources={r"/api/*": {"origin": "*"}})
 
 @app.route('/')
 def check():
     return "¡Todo good!"
 
-@app.route('/api/login', methods=['GET'])
-def login():
-    user = request.args.get('user')
-    pwd = request.args.get('pwd')
-    return mysql.login(user, pwd)
+
+@app.route('/login',methods=['POST'])
+def iniciarsesion():
+    data = request.json
+    user = data['user']
+    pwd = data['pwd']
+    cur = mysql.connection.cursor()
+    cur.execute('select * from Usuario where usuario=\''+user+'\' and contraseña=\''+pwd+'\'')
+    cur.execute('''SELECT * from Usuario''')
+    rv = cur.fetchall()
+    for x in rv:
+        if x[2]== user and x[3]==pwd:
+            return "{\"data\":\""+x[1]+"\"}"
+    return "{\"code\":\"false\"}"
+
+    
+
+
+@app.route('/registro',methods=['POST'])
+def registro():
+    dato=request.json
+    name=dato['name']
+    user=dato['user']
+    pasw=dato['pasw']
+    cur = mysql.connection.cursor()
+    cur.execute('insert into Usuario(nombre,usuario,contraseña) values(\''+name+'\',\''+user+'\',\''+pasw+'\')')
+    mysql.connection.commit()
+    cur.close()
+    return "{\"data\":\"true\"}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=True)
--- backend/mysql.py
+++ backend/mysql.py
@@ -2,15 +2,15 @@ import pymysql
 import pymysql.cursors
 
 class Mysql:
-	SQL = "CONSULTA"	#COLOCAR CONSULTA AQUÍ
+	SQL = "select * from Usuario"	#COLOCAR CONSULTA AQUÍ
 
 	def __init__(self):
 		self.connection = pymysql.connect(
 		# host="localhost", 
-		host='172.19.0.4',
-		user="root", 
-		passwd="12345678", 
-		db="P2Bases", 
+		host='34.72.187.239',
+		user="admin", 
+		passwd="admin", 
+		db="Sopes1", 
 		cursorclass=pymysql.cursors.DictCursor, 
 		sql_mode=''
 	)
@@ -18,6 +18,20 @@ class Mysql:
 	def consulta(self, num):
 		with self.connection.cursor() as cursor:
 			cursor.execute(self.SQL)
+			res = cursor.fetchall()
+		self.connection.close()
+		return res
+
+	def login(self,usr1,pasw):
+		with self.connection.cursor() as cursor:
+			cursor.execute('select * from Usuario where usuario=\''+usr1+'\' and contraseña=\''+pasw+'\'')
+			res = cursor.fetchall()
+		self.connection.close()
+		return res
+
+	def register(self,name,user,pasw):
+		with self.connection.cursor() as cursor:
+			cursor.execute('insert into Usuario(nombre,usuario,contraseña) values(\''+name+'\',\''+user+'\',\''+pasw+'\')')
 			res = cursor.fetchall()
 		self.connection.close()
 		return res
